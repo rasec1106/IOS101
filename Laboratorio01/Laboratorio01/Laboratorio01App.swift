8298917b24dc19c3d11cc1c28df755cb05bae6a9
Suite <suite@SUITESB505-ST04.local>
2023-08-24 21:20:08 -0500

    Create empty project

---
+++ Laboratorio01/Laboratorio01/Laboratorio01App.swift
@@ -0,0 +1,17 @@
+//
+//  Laboratorio01App.swift
+//  Laboratorio01
+//
+//  Created by Suite on 24/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Laboratorio01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
