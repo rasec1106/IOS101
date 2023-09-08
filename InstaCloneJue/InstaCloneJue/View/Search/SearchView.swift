1d432e55caeac655e325fa7d57a067ffd365bf97
Suite <suite@SUITESB505-ST20.local>
2023-09-07 22:21:38 -0500

    Add searchable

--- InstaCloneJue/InstaCloneJue/View/Search/SearchView.swift
+++ InstaCloneJue/InstaCloneJue/View/Search/SearchView.swift
@@ -1,29 +1,30 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var search: String = "" // this State annotation is to mantain the state when the view rerenders (or smthg like that)
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(0...18,id: \.self) { contact in
-                    HStack {
-                        CircleImage(image: "Batman", width: 40, height: 40)
-                        VStack(alignment: .leading) {
-                            Text("Batman")
-                                .fontWeight(.semibold)
-                            Text("Bruce Wayne")
-                                .font(.footnote)
-                        }
-                        Spacer()
-                    } // end of HStack
-                    .padding(.leading, 8)
-                } // end of ForEach
-            } // end of LazyVStack
-        } // end of ScrollView
-    }
-}
-
-struct SearchView_Previews: PreviewProvider {
-    static var previews: some View {
-        SearchView()
+        NavigationView {
+            ScrollView {
+                LazyVStack {
+                    ForEach(0...18,id: \.self) { contact in
+                        HStack {
+                            CircleImage(image: "Batman", width: 40, height: 40)
+                            VStack(alignment: .leading) {
+                                Text("Batman")
+                                    .fontWeight(.semibold)
+                                Text("Bruce Wayne")
+                                    .font(.footnote)
+                            }
+                            Spacer()
+                        } // end of HStack
+                        .padding(.leading, 8)
+                    } // end of ForEach
+                } // end of LazyVStack
+                .searchable(text: $search) // to create the searchbox (***to be reviewed in the next lesson***)
+            } // end of ScrollView
+        }
+        .navigationTitle("Explore")
+        .navigationBarTitleDisplayMode(.inline)
+        
     }
 }
