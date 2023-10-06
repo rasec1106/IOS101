f4032fe6d09f743a2c0ade6fa8468504fa83f480
rasec1106 <elch.herrera@gmail.com>
2023-10-05 21:36:50 -0500

    Create CurrentUserProfileView

---
+++ InstaCloneJue/InstaCloneJue/View/Profile/CurrentUserProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  CurrentUserProfileView.swift
+//  InstaCloneJue
+//
+//  Created by Suite on 5/10/23.
+//
+
+import SwiftUI
+
+struct CurrentUserProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CurrentUserProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        CurrentUserProfileView()
+    }
+}
