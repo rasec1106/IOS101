ece0c2a74c488cde50ff342f5df94dd69741a8fd
rasec1106 <elch.herrera@gmail.com>
2023-10-05 22:02:44 -0500

    Add navigation logic from Search to ProfileView

--- InstaCloneJue/InstaCloneJue/View/Profile/ProfileView.swift
+++ InstaCloneJue/InstaCloneJue/View/Profile/ProfileView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 struct ProfileView: View {
+    let user: User
     private let columns = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
     var body: some View {
         VStack {
@@ -8,7 +9,7 @@ struct ProfileView: View {
             VStack {
                 // Pic and stats
                 HStack {
-                    CircleImage(image: "BlackPanther", width: 80, height: 80)
+                    CircleImage(image: user.profileImageUrl ?? "", width: 80, height: 80)
                     Spacer()
                     UserStatsView(value: "3", name: "Posts")
                     Spacer()
@@ -19,38 +20,22 @@ struct ProfileView: View {
                 
                 // Name and nickname
                 VStack(alignment: .leading, spacing: 8) {
-                    Text("Cesar Herrera")
+                    Text(user.fullname ?? "Username")
                         .font(.footnote)
                         .fontWeight(.semibold)
-                    Text("rasec")
+                    Text(user.nickname ?? "Nickname")
                         .font(.footnote)
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
-                
-                // Button
-                Button {
-                    
-                } label: {
-                    Text("Edit Profile")
-                        .font(.subheadline)
-                        .fontWeight(.bold)
-                        .foregroundColor(.black)
-                        .frame(width: 360,height: 32)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 6)
-                                .stroke(Color.gray,lineWidth: 1)
-                        )
-                }
-                
-
             }
             .padding(.horizontal)
-            
             // Post
             ScrollView {
                 LazyVGrid(columns: columns) {
-                    ForEach(0...15, id:\.self) {
-                        index in Image("BlackPanther").resizable().scaledToFill()
+                    ForEach(0...15, id:\.self) { index in
+                        Image(user.profileImageUrl ?? "")
+                            .resizable()
+                            .scaledToFill()
                     }
                 }
             }
@@ -65,11 +50,5 @@ struct ProfileView: View {
             }
         }
         
-    }
-}
-
-struct ProfileView_Previews: PreviewProvider {
-    static var previews: some View {
-        ProfileView()
     }
 }
--- InstaCloneJue/InstaCloneJue/View/Search/SearchView.swift
+++ InstaCloneJue/InstaCloneJue/View/Search/SearchView.swift
@@ -3,28 +3,34 @@ import SwiftUI
 struct SearchView: View {
     @State private var search: String = "" // this State annotation is to mantain the state when the view rerenders (or smthg like that)
     var body: some View {
-        NavigationView {
+        NavigationStack {
             ScrollView {
                 LazyVStack {
                     ForEach(User.MOCK_USERS) { user in
-                        HStack {
-                            CircleImage(image: user.profileImageUrl ?? "", width: 40, height: 40)
-                            VStack(alignment: .leading) {
-                                Text(user.username)
-                                    .fontWeight(.semibold)
-                                Text(user.fullname ?? "User")
-                                    .font(.footnote)
-                            }
-                            Spacer()
-                        } // end of HStack
-                        .padding(.leading, 8)
+                        NavigationLink(value: user) {
+                            HStack {
+                                CircleImage(image: user.profileImageUrl ?? "", width: 40, height: 40)
+                                VStack(alignment: .leading) {
+                                    Text(user.username)
+                                        .fontWeight(.semibold)
+                                    Text(user.fullname ?? "User")
+                                        .font(.footnote)
+                                }
+                                Spacer()
+                            } // end of HStack
+                            .padding(.leading, 8)
+                            .foregroundColor(.black)
+                        } // end of NavigationLink
                     } // end of ForEach
                 } // end of LazyVStack
                 .searchable(text: $search, prompt: "Search...") // to create the searchbox (***to be reviewed in the next lesson***)
             } // end of ScrollView
+            // This navigationDestination is in the ScrollView because for navigation we need the parent container of the NavigationLink
+            .navigationDestination(for: User.self, destination: { user in
+                ProfileView(user: user)
+            })
+            .navigationTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .navigationTitle("Explore")
-        .navigationBarTitleDisplayMode(.inline)
-        
     }
 }
