a83bc6722399b503022f1b8d424bb043b6d32633
rasec1106 <elch.herrera@gmail.com>
2023-10-05 22:47:22 -0500

    Add firebase setup

--- InstaCloneJue/InstaCloneJue/InstaCloneJueApp.swift
+++ InstaCloneJue/InstaCloneJue/InstaCloneJueApp.swift
@@ -6,10 +6,26 @@
 //
 
 import SwiftUI
+/*
+ Add some code for firebase setup
+ */
+import FirebaseCore
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+// end of code
 
 @main
 struct InstaCloneJueApp: App {
     var body: some Scene {
+        // register app delegate for Firebase setup
+        @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
         WindowGroup {
             // ContentView()
             MainTabView()
